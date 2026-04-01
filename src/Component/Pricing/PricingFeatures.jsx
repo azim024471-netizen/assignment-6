a083be408266e73d0f47b7fce675fea0d321a9a1
azim024471-netizen <azim024471@gmail.com>
2026-04-01 23:55:38 +0600

    fixed the pricing features colour

--- src/Component/Pricing/PricingFeatures.jsx
+++ src/Component/Pricing/PricingFeatures.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import checkIcon from '../../assets/Check.png';
 
-const PricingFeatures = ({feature}) => {
+const PricingFeatures = ({feature,popular}) => {
+
     return (
          <div className="flex items-center gap-1">
             <img src={checkIcon} alt="check" className="w-5 h-5 object-contain" />
-            <p className="text-gray-600 font-medium"> {feature} </p>
+            <p className={` font-medium ${
+                popular === true ? "text-white" : "text-gray-600"
+            }`}> {feature} </p>
         </div>
     );
 };
